01ffa0bab875ebb1782b3a5fd1924ef02f41916b
Sailesh Nepal <nepalsailesh10@gmail.com>
2023-04-26 21:01:57 +0545

    end of v-6-crispy forms

--- django_project/settings.py
+++ django_project/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    "crispy_bootstrap5",
+    
 ]
 
 MIDDLEWARE = [
@@ -129,3 +132,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 #self written
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
